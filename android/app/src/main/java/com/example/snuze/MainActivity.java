864d1fb1f897741c17332c9b24fe2a3d9b3a24c8
Weston Harper <wes@tao.team>
2018-11-22 13:21:50 -0700

    feat(android): Update android platform to accept stripe message and generate token
    re #17

--- android/app/src/main/java/com/example/snuze/MainActivity.java
+++ android/app/src/main/java/com/example/snuze/MainActivity.java
@@ -9,9 +9,15 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import android.util.Log;
 import android.content.Context;
+import android.widget.Toast;
+
 import com.stripe.android.Stripe;
 import com.stripe.android.model.Token;
+import com.stripe.android.model.Card;
+import com.stripe.android.TokenCallback;
 import org.json.JSONObject;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends FlutterActivity {
   private static final String CHANNEL = "snuze.app/stripe";
@@ -25,31 +31,32 @@ public class MainActivity extends FlutterActivity {
     new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
       new MethodCallHandler() {
         @Override
-        public void onMethodCall(MethodCall call, Result result) {
+        public void onMethodCall(MethodCall call, final Result result) {
 //        System.out.println(call);
           if (call.method.equals("createStripeToken")) {
               System.out.println(call.arguments().toString());
 
                Card card = new Card("4242424242424242", 11, 2020, "123");
-//               if (!card.validateCard()) {
-//                   System.out.println("invalid card format");
-//               }
-//
-//              stripe.createToken(
-//                      card,
-//                      new TokenCallback() {
-//                          public void onSuccess(Token token) {
-//                              result.success(token.tokenId);
-//                          }
-//                          public void onError(Exception error) {
-//                              // Show localized error message
+               if (!card.validateCard()) {
+                   System.out.println("invalid card format");
+               }
+
+              stripe.createToken(
+                      card,
+                      new TokenCallback() {
+                          public void onSuccess(Token token) {
+                              result.success(token.getId());
+                          }
+                          public void onError(Exception error) {
+                              // Show localized error message
 //                              Toast.makeText(getContext(),
-//                                      error.getLocalizedString(getContext()),
+////                                      error.getLocalizedString(getContext()),
 //                                      Toast.LENGTH_LONG
 //                              ).show();
-//                          }
-//                      }
-//              );
+                              result.error("STRIPE_TOKEN", "error creating stripe token", null);
+                          }
+                      }
+              );
 
 
           } else {

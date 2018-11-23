a818429f8b7f8f4afc58d47f2b0cb8c66fbe45ef
Weston Harper <wes@tao.team>
2018-11-23 14:47:29 -0700

    feat(android): Update Android stripe integration to use user input properly
    re #17

--- android/app/src/main/java/com/example/snuze/MainActivity.java
+++ android/app/src/main/java/com/example/snuze/MainActivity.java
@@ -8,23 +8,43 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-import android.util.JsonReader;
-import android.util.Log;
-import android.content.Context;
-import android.widget.Toast;
-
 import com.stripe.android.Stripe;
 import com.stripe.android.model.Token;
 import com.stripe.android.model.Card;
 import com.stripe.android.TokenCallback;
-import org.json.JSONObject;
 
-import static java.security.AccessController.getContext;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends FlutterActivity {
   private static final String CHANNEL = "snuze.app/stripe";
   Stripe stripe = new Stripe(this, "pk_test_YhN3AX1KBNqQQbDtGjctrCZd");
-  
+
+    public JSONObject newJSONObject(String string) {
+        try {
+            JSONObject result = new JSONObject(string);
+            return result;
+        } catch (JSONException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Card createCard(JSONObject cardInfo) {
+        try {
+            Card result = new Card(
+                    cardInfo.getString("number"),
+                    cardInfo.getInt("expMonth"),
+                    cardInfo.getInt("expYear"),
+                    cardInfo.getString("cvc")
+            );
+            return result;
+        } catch (JSONException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -34,30 +54,23 @@ public class MainActivity extends FlutterActivity {
       new MethodCallHandler() {
         @Override
         public void onMethodCall(MethodCall call, final Result result) {
-//        System.out.println(call);
+        System.out.println(call.arguments().toString());
+        JSONObject cardInfo = newJSONObject(call.arguments().toString());
+        System.out.println(cardInfo);
           if (call.method.equals("createStripeToken")) {
-               Card card = new Card(
-                   call.argument("number").toString(),
-                   (Integer) call.argument("expMonth"),
-                   (Integer) call.argument("expYear"),
-                   call.argument("cvc").toString()
-               );
+               Card card = createCard(cardInfo);
                if (!card.validateCard()) {
                    System.out.println("invalid card format");
+                   result.error("INVALID_CARD", "card not valid", null);
                }
 
               stripe.createToken(
                       card,
                       new TokenCallback() {
                           public void onSuccess(Token token) {
-                              result.success(token.getId());
+                              result.success(token.getId().toString());
                           }
                           public void onError(Exception error) {
-                              // Show localized error message
-//                              Toast.makeText(getContext(),
-////                                      error.getLocalizedString(getContext()),
-//                                      Toast.LENGTH_LONG
-//                              ).show();
                               result.error("STRIPE_TOKEN", "error creating stripe token", null);
                           }
                       }

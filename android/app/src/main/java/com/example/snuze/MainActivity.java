4d9fb98f9483f8a7a13e08c786c286e7fa54c156
Weston Harper <wes@tao.team>
2018-11-29 11:53:32 -0700

    feat(Android Alarm): Update alarm permissions and start alarm setting functionality
    SET_ALARM currently opens default android alarm app, needs to be fixed.
    
    re #16

--- android/app/src/main/java/com/example/snuze/MainActivity.java
+++ android/app/src/main/java/com/example/snuze/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.snuze;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
@@ -21,9 +27,10 @@ public class MainActivity extends FlutterActivity {
 
     private static final String STRIPE_CHANNEL = "snuze.app/stripe";
     private static final String ALARM_CHANNEL = "snuze.app/alarm";
+    private static final int MY_PERMISSIONS_REQUEST_SET_ALARM = 17;
     Stripe stripe = new Stripe(this, "pk_test_YhN3AX1KBNqQQbDtGjctrCZd");
 
-    public JSONObject newJSONObject(String string) {
+    private JSONObject newJSONObject(String string) {
         try {
             JSONObject result = new JSONObject(string);
             return result;
@@ -33,7 +40,7 @@ public class MainActivity extends FlutterActivity {
         }
     }
 
-    public Card createCard(JSONObject cardInfo) {
+    private Card createCard(JSONObject cardInfo) {
         try {
             Card result = new Card(
                     cardInfo.getString("number"),
@@ -48,10 +55,62 @@ public class MainActivity extends FlutterActivity {
         }
     }
 
+    private Intent createAlarmIntent(JSONObject alarmInfo) {
+        try {
+            Intent alarmIntent = new Intent(AlarmClock.ACTION_SET_ALARM);
+            alarmIntent.putExtra(AlarmClock.EXTRA_HOUR, alarmInfo.getInt("hour"));
+            alarmIntent.putExtra(AlarmClock.EXTRA_HOUR, alarmInfo.getInt("minute"));
+            return alarmIntent;
+        } catch (JSONException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Intent dismissAlarmIntent() {
+        Intent alarmIntent = new Intent(AlarmClock.ACTION_DISMISS_ALARM);
+        return alarmIntent;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SET_ALARM: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         GeneratedPluginRegistrant.registerWith(this);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SET_ALARM) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.SET_ALARM)) {
+                System.out.println("needs explanation");
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.SET_ALARM},
+                        MY_PERMISSIONS_REQUEST_SET_ALARM);
+            }
+        } else {
+            // Permission already granted
+        }
 
         new MethodChannel(getFlutterView(), STRIPE_CHANNEL).setMethodCallHandler(
                 new MethodCallHandler() {
@@ -90,11 +149,12 @@ public class MainActivity extends FlutterActivity {
                     @Override
                     public void onMethodCall(MethodCall call, Result result) {
                         if (call.method.equals("setAlarm")) {
-                            System.out.println("SETTING ALARM");
-                            System.out.println(call.arguments().toString());
+                            JSONObject alarmInfo = newJSONObject(call.arguments().toString());
+                            Intent alarmIntent = createAlarmIntent(alarmInfo);
+                            startActivity(alarmIntent);
                         } else if (call.method.equals("cancelAlarm")) {
-                            System.out.println("CANCELLING ALARM");
-                            System.out.println(call.arguments().toString());
+                            Intent alarmIntent = dismissAlarmIntent();
+                            startActivity(alarmIntent);
                         }
                     }
                 }
